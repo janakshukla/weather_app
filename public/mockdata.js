7250241af381014fef98e5ef103bb24a6b6e05da
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-03-28 16:11:40 +0530

    add global styles, button, input components, utility functions, mock data, and SVG assets

---
+++ public/mockdata.js
@@ -0,0 +1,46 @@
+const mockData =
+
+  [
+
+    {
+      "weather": "Clear Sky",
+      "imageUrl": "https://images.unsplash.com/photo-1601297183305-6df142704ea2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "weather": "Few Clouds",
+      "imageUrl": "https://images.unsplash.com/photo-1465156799763-2c087c332922?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+      "weather": "overcast clouds",
+      "imageUrl": "https://images.unsplash.com/photo-1642447733831-2cdd9f5efae7?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "weather": "Broken Clouds",
+      "imageUrl": "https://images.unsplash.com/photo-1515868433262-4c9af9857b45?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "weather": "Shower Rain",
+      "imageUrl": "https://images.unsplash.com/photo-1494007485290-ce668e189d92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "weather": "Rain",
+      "imageUrl": "https://images.unsplash.com/photo-1501691223387-dd0500403074?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8UmFpbnxlbnwwfHwwfHx8MA%3D%3D"
+    },
+
+    {
+      "weather": "Thunderstorm",
+      "imageUrl": "https://images.unsplash.com/photo-1605727216801-e27ce1d0cc28?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8VGh1bmRlcnN0b3JtfGVufDB8fDB8fHww"
+    },
+    {
+      "weather": "Snow",
+      "imageUrl": "https://plus.unsplash.com/premium_photo-1663090593977-9923cc536f3b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8U25vd3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+      "weather": "Mist",
+      "imageUrl": "https://images.unsplash.com/photo-1485236715568-ddc5ee6ca227?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8TWlzdHxlbnwwfHwwfHx8MA%3D%3D"
+    }
+  ]
+
+
+  export default mockData;
